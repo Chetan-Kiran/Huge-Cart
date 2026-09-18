9a1e709ab46b6017faa3ceeb0297168a83de1f51
Chetan_Kiran <chetankiran9666@gmail.com>
2026-09-18 08:46:16 +0530

    Add firstName and lastName to OrderPlacedEvent

--- notification-service/src/main/java/com/techie/microservices/notification/order/event/OrderPlacedEvent.java
+++ notification-service/src/main/java/com/techie/microservices/notification/order/event/OrderPlacedEvent.java
@@ -4,13 +4,20 @@ public class OrderPlacedEvent {
 
     private String orderNumber;
     private String email;
+    private String firstName;
+    private String lastName;
 
     public OrderPlacedEvent() {
     }
 
-    public OrderPlacedEvent(String orderNumber, String email) {
+    public OrderPlacedEvent(String orderNumber,
+                            String email,
+                            String firstName,
+                            String lastName) {
         this.orderNumber = orderNumber;
         this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getOrderNumber() {
@@ -27,5 +34,21 @@ public class OrderPlacedEvent {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
