6cd547ad3ec3c386af489e944ef51475d9879c9d
Chetan_Kiran <chetankiran9666@gmail.com>
2026-09-29 12:00:00 +0530

    Debug && Deploy ready

--- frontend-react/src/services/inventoryApi.jsx
+++ frontend-react/src/services/inventoryApi.jsx
@@ -1,16 +1,17 @@
 import axios from "./axiosConfig";
 
-const API = "/api/inventory";
+const BASE = "/api/inventory";
 
 export const getInventory = async (skuCode) => {
-  const response = await axios.get(`${API}/${skuCode}`);
+  const response = await axios.get(`${BASE}/${skuCode}`);
   return response.data;
 };
 
 export const updateInventoryQuantity = async (skuCode, quantity) => {
-  const response = await axios.post(`${API}/updateQuantity`, {
+  const response = await axios.post(`${BASE}/updateQuantity`, {
     skuCode,
     quantity,
   });
+
   return response.data;
 };
--- frontend-react/src/services/orderApi.jsx
+++ frontend-react/src/services/orderApi.jsx
@@ -2,13 +2,11 @@ import axios from "./axiosConfig";
 
 const API = "/api/order";
 
-// PLACE ORDER
 export const placeOrder = async (order) => {
   const response = await axios.post(API, order);
   return response.data;
 };
 
-// GET ALL ORDERS
 export const getAllOrders = async () => {
   const response = await axios.get(API);
   return response.data;
