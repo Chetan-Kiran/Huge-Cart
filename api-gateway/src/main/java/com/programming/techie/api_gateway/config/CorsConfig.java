4959dd6f1fe142435fa380849352d40625e4f8f0
Chetan_Kiran <chetankiran9666@gmail.com>
2026-09-29 12:00:00 +0530

    Debug && Deploy ready (api-gateway fix)

--- api-gateway/src/main/java/com/programming/techie/api_gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/programming/techie/api_gateway/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173",
-                    "https://hugecart-frontend.onrender.com/"
+                    "https://hugecart-frontend.onrender.com"
                 )
                 .allowedMethods("*")
                 .allowedHeaders("*")
